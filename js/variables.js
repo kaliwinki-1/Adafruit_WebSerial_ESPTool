69e941e25632078987cd623ba63683b9d57e3111
KALIWINKI <kaliwinki06@gmail.com>
2026-03-26 01:40:06 -0400

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,28 +1,24 @@
 // ================================================
-// js/variables.js - VERSION SIMPLIFIÉE CYD2USB
+// js/variables.js - VERSION FINALE POUR TOI
 // ================================================
 
-// Fichiers communs pour toutes les variantes CYD2USB
 const commonBootloader = 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin';
 const commonPartitions = 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin';
 
-// ====================== VARIANTS CYD2USB ======================
+// ====================== TES 3 VARIANTS SEULEMENT ======================
 
-// Marauder CYD2USB
 const MCYD2USBMarauderFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
     'firmware': 'resources/CURRENT/marauder.bin',
 };
 
-// HaleHound CYD2USB
 const MCYD2USBHaleHoundFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
     'firmware': 'resources/CURRENT/halehound.bin',
 };
 
-// Bruce CYD2USB
 const MCYD2USBBruceFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
