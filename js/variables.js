f5541fb4a11cb7a9868321df8c7d62c089bb204d
KALIWINKI <kaliwinki06@gmail.com>
2026-03-26 02:27:49 -0400

    Refactor file exports for MCYD2USB configurations

--- js/variables.js
+++ js/variables.js
@@ -1,20 +1,17 @@
-// Groupe pour Marauder
-const MCYD2USBMarauderFiles = {
+export const MCYD2USBMarauderFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/ton_firmware_marauder.bin', // <--- TON FICHIER ICI
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_11_0_cyd2usb.bin',
 };
 
-// Groupe pour HaleHound
-const MCYD2USBHaleHoundFiles = {
+export const MCYD2USBHaleHoundFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/ton_firmware_halehound.bin', // <--- TON FICHIER ICI
+    'firmware': 'resources/CURRENT/halehound_cyd2usb.bin',
 };
 
-// Groupe pour Bruce
-const MCYD2USBBruceFiles = {
+export const MCYD2USBBruceFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/ton_firmware_bruce.bin', // <--- TON FICHIER ICI
+    'firmware': 'resources/CURRENT/bruce_cyd2usb.bin',
 };
