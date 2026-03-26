7a14bb0794a088f2722db2abc7510ae98035f8b9
KALIWINKI <kaliwinki06@gmail.com>
2026-03-26 02:39:15 -0400

    Modify firmware paths in variables.js
    Updated firmware file paths for Marauder, Hale Hound, and Bruce.

--- js/variables.js
+++ js/variables.js
@@ -1,18 +1,18 @@
-// Définition globale des firmwares pour le CYD2USB
+// Configuration globale avec les noms de fichiers exacts
 const MCYD2USBMarauderFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_11_0_cyd2usb.bin'
+    'firmware': 'resources/CURRENT/marauder.bin'
 };
 
 const MCYD2USBHaleHoundFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/halehound_cyd2usb.bin'
+    'firmware': 'resources/CURRENT/halehound.bin'
 };
 
 const MCYD2USBBruceFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/bruce_cyd2usb.bin'
+    'firmware': 'resources/CURRENT/bruce.bin'
 };
