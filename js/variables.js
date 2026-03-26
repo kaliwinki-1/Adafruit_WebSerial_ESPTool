915f71f6015dc6e9a47d42f89396302d0a69c2b8
KALIWINKI <kaliwinki06@gmail.com>
2026-03-26 02:33:29 -0400

    Refactor firmware definitions for CYD2USB

--- js/variables.js
+++ js/variables.js
@@ -1,17 +1,18 @@
-export const MCYD2USBMarauderFiles = {
+// Définition globale des firmwares pour le CYD2USB
+const MCYD2USBMarauderFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_11_0_cyd2usb.bin',
+    'firmware': 'resources/CURRENT/esp32_marauder_v1_11_0_cyd2usb.bin'
 };
 
-export const MCYD2USBHaleHoundFiles = {
+const MCYD2USBHaleHoundFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/halehound_cyd2usb.bin',
+    'firmware': 'resources/CURRENT/halehound_cyd2usb.bin'
 };
 
-export const MCYD2USBBruceFiles = {
+const MCYD2USBBruceFiles = {
     'bootloader': 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin',
     'partitions': 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin',
-    'firmware': 'resources/CURRENT/bruce_cyd2usb.bin',
+    'firmware': 'resources/CURRENT/bruce_cyd2usb.bin'
 };
