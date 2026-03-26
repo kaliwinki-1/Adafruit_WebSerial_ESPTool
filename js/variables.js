7b45dbcc9fcf818991b70545bbf0d793a969db6f
KALIWINKI <kaliwinki06@gmail.com>
2026-03-26 01:27:34 -0400

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,30 +1,30 @@
 // ================================================
-// js/variables.js - VERSION SIMPLIFIÉE POUR CYD2USB
+// js/variables.js - VERSION SIMPLIFIÉE CYD2USB
 // ================================================
 
-// Fichiers communs (bootloader + partitions) pour toutes les variantes CYD2USB
+// Fichiers communs pour toutes les variantes CYD2USB
 const commonBootloader = 'resources/STATIC/M/CYD/esp32_marauder.ino.bootloader.bin';
 const commonPartitions = 'resources/STATIC/M/CYD/esp32_marauder.ino.partitions.bin';
 
-// ====================== TES 3 VARIANTS CYD2USB ======================
+// ====================== VARIANTS CYD2USB ======================
 
 // Marauder CYD2USB
 const MCYD2USBMarauderFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
-    'firmware': 'resources/CURRENT/esp32_marauder_v1_4_3_20250416_cyd2usb_gps.bin',
+    'firmware': 'resources/CURRENT/marauder.bin',
 };
 
 // HaleHound CYD2USB
 const MCYD2USBHaleHoundFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
-    'firmware': 'resources/CURRENT/NOM_DU_FICHIER_HALEHOUND.bin',   // ← À MODIFIER
+    'firmware': 'resources/CURRENT/halehound.bin',
 };
 
 // Bruce CYD2USB
 const MCYD2USBBruceFiles = {
     'bootloader': commonBootloader,
     'partitions': commonPartitions,
-    'firmware': 'resources/CURRENT/NOM_DU_FICHIER_BRUCE.bin',       // ← À MODIFIER
+    'firmware': 'resources/CURRENT/bruce.bin',
 };
